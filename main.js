91a800fe0eb6580ba158c92f2a8e48ed258c34b4
Paulina Goral-Guntuku <goral.paulina@gmail.com>
2020-05-14 20:42:24 +0200

    Added text to HTML using JS

--- main.js
+++ main.js
@@ -5,3 +5,6 @@ console.log(firstName);
 console.log(age);
 
 console.log(`Cześć. Mam na imię ${firstName} i mam ${age} lat.`);
+
+const paragraph = document.querySelector('.homework--js');
+paragraph.innerHTML = 'Zadanie domowe z JS wykonane!';
