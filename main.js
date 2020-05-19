ba49583a0765fa54d97380e1f610a40355f57675
Paulina Goral-Guntuku <goral.paulina@gmail.com>
2020-05-19 21:26:52 +0200

    Week 5 JS homework

--- main.js
+++ main.js
@@ -8,3 +8,9 @@ console.log(`Cześć. Mam na imię ${firstName} i mam ${age} lat.`);
 
 const emptyParagraph = document.querySelector('.homework--js');
 emptyParagraph.innerHTML = 'Zadanie domowe z JS wykonane!';
+
+const greet = (age1, name1) => {
+    console.log(`Witaj Drogi Odwiedzający, mam na imię ${name1} i mam ${age1} lat.`);
+}
+
+greet(28, 'Paulina')
